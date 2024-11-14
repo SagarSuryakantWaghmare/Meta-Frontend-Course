92233cb38f3ee7d5bb2d0ef6655c45620b38affe
Sagar <sagarwaghmare1384@gmail.com>
2024-11-14 11:07:28 +0530

    Components completed

---
+++ React Basics/first/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Nav(){
+    <Nav className="main-nav">
+        <ul>
+            <li>Home</li>
+            <li>Articles</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </Nav>
+}
+export default Nav;
