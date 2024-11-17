c0c8305f38fe696ed8776df9b1be894ac4bcb9d2
Sagar <sagarwaghmare1384@gmail.com>
2024-11-17 21:10:04 +0530

    Rendering the pages

--- React Basics/adv/src/pages/About.js
+++ React Basics/adv/src/pages/About.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 function About() {
+    let name="Sagar";
+    let test=true;
+    const day=new Date().getDay;
   return (
     <>
-    
     <div>About</div>
+    {name.length===5?<h1>You must be smarter than all</h1>:<h2>You must be change your name</h2>}
+    <div>{test && <h1>Hello</h1>} World {day}</div >
+    
     
     </>
   )
