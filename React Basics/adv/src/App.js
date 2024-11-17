063b26e5c3cc32b8b9f6047cd9b6b19ebb16a470
Sagar <sagarwaghmare1384@gmail.com>
2024-11-17 21:31:54 +0530

    Assets fixing

--- React Basics/adv/src/App.js
+++ React Basics/adv/src/App.js
@@ -2,12 +2,14 @@
 import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Image from './pages/Image'
 function App() {
   return (
    <>
    <Routes>
    <Route path='/home' element={<Home/>} />
    <Route path='/about' element={<About/>} />
+   <Route path='/image' element={<Image/>} />
    </Routes>
    </>
   );
---
+++ React Basics/adv/src/pages/Image.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sagar from '../assets/Sagar.jpg'
+function Image() {
+  return (
+    <>
+    <h1>Sagar Suryakant Waghmare</h1>
+    <p>My mind Set is clear about my aim</p>
+    <img src={Sagar} height={600} alt="Sagar"></img>
+    </>
+  )
+}
+
+export default Image
