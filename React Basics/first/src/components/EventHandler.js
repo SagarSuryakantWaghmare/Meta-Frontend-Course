5bfb3e3a0bbc899673c904d888034309c0fcc1ab
Sagar <sagarwaghmare1384@gmail.com>
2024-11-17 11:28:51 +0530

    Making the commit proper

--- React Basics/first/src/components/EventHandler.js
+++ React Basics/first/src/components/EventHandler.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function EventHandler() {
-   const[count,setCount]=useState(0);
-  function handleClick(){
-    console.log("The button clicked")
-    setCount(count+1);
+  const [count, setCount] = useState(0);
 
-
+  function handleClick() {
+    console.log("The button was clicked");
+    setCount(count + 1);
   }
-  return (
-    <>    <button className="text-2xl border-b-orange-600" onClick={handleClick}>Increased the power of the dino</button>
-    <div>{count}</div>
-    </>
 
-  )
+  function handlePowerUp() {
+    console.log("Sagar will never give up on his dreams. That is his ultimate way!");
+    alert("Sagar will never give up on his dreams. That is his ultimate way!");
+  }
+
+  return (
+    <>
+      <button className="text-2xl border-b-orange-600" onClick={handleClick}>
+        Increase the power of the dino
+      </button>
+      <div>{count}</div>
+      <button onClick={handlePowerUp}>Power up</button>
+    </>
+  );
 }
 
-export default EventHandler 
+export default EventHandler;
