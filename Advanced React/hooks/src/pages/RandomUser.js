25357e753a8831c7e2a6bbf29272750c1732b296
Sagar <sagarwaghmare1384@gmail.com>
2024-11-24 19:57:01 +0530

    Adding css to the random user

--- Advanced React/hooks/src/pages/RandomUser.js
+++ Advanced React/hooks/src/pages/RandomUser.js
@@ -1,27 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import './RandomUser.css'; // Importing the CSS file
 
 function RandomUser() {
-    const[user,setUser]=useState(null);
-    const FetchData=()=>{
-        fetch('https://randomuser.me/api/?results=1')
-        .then(response=>response.json())
-        .then(console.log(user))
-        .then(data => setUser(data.results[0]))
-        .catch((error)=>console.log(error))
-    }
-    useEffect(()=>{
-        FetchData();
-    },[])
-  return user && (
-    <>
-    <div>RandomUser</div>
-    <h4>Data of the random user</h4>
-    <h1>name of the user{user.name.first} {user.name.last}</h1>
-    <p>Email of the user is {user.email}</p>
-    <p>Gender of the user is {user.gender}</p>
-    <p>Age of the user is  {user.dob.age}</p>
-    </>
-  )
+  const [user, setUser] = useState(null);
+
+  const FetchData = () => {
+    fetch('https://randomuser.me/api/?results=1')
+      .then((response) => response.json())
+      .then((data) => setUser(data.results[0]))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    FetchData();
+  }, []);
+
+  return (
+    user && (
+      <div className="random-user-container">
+        <div className="random-user-header">Random User</div>
+        <h4>Data of the Random User</h4>
+        <div className="user-details">
+          <h1 className="user-name">
+            {user.name.first} {user.name.last}
+          </h1>
+          <p className="user-info">Email: {user.email}</p>
+          <p className="user-info">Gender: {user.gender}</p>
+          <p className="user-info">Age: {user.dob.age}</p>
+        </div>
+      </div>
+    )
+  );
 }
 
-export default RandomUser
+export default RandomUser;
